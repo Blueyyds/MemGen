9f4451f150e202482d3fb1a0638a98d4a555d367
Blueyyds <2410104033@mails.szu.edu.cn>
2025-11-16 17:29:33 +0800

    feat: support custom save dir.

--- main.py
+++ main.py
@@ -13,20 +13,22 @@ from larm.common.logger import setup_logger
 from larm.common.registry import registry
 from larm.task import Task, BaseRunner
 
+
 def set_seed(random_seed: int, use_gpu: bool):
 
     random.seed(random_seed)
-    os.environ['PYTHONHASHSEED'] = str(random_seed)
+    os.environ["PYTHONHASHSEED"] = str(random_seed)
     np.random.seed(random_seed)
     torch.manual_seed(random_seed)
     torch.cuda.manual_seed(random_seed)
     if use_gpu:
         torch.cuda.manual_seed_all(random_seed)
 
-    torch.backends.cudnn.deterministic = True   
-    torch.backends.cudnn.benchmark = False      
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     print(f"set seed: {random_seed}")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Language Reasoning and Memory")
@@ -44,20 +46,25 @@ def parse_args():
 
     return args
 
+
 def get_save_dir(config) -> str:
+    if config.run_cfg.save_dir is not None:
+        return config.run_cfg.save_dir
     time = datetime.now().strftime("%Y%m%d-%H%M%S")
     return os.path.join("results", config.method, time)
+
 
 def get_runner_class(config) -> BaseRunner:
     print(config.method)
     return registry.get_runner_class(config.method)
 
+
 def main():
-    
+
     # parse configs
     args = parse_args()
     config = Config(args)
-    
+
     set_seed(config.run_cfg.seed, use_gpu=True)
 
     # set up save folder
@@ -69,27 +76,28 @@ def main():
     setup_logger(output_dir=config.run_cfg.log_dir)
 
     config.pretty_print()
-    
+
     task = Task(config)
     datasets_dict = task.build_dataset()
     env_and_gens_dict = task.build_env_and_generator()
     model = task.build_model()
-    
+
     # build runner
     runner_cls = get_runner_class(config)
     runner = runner_cls(
-        model=model, 
-        processing_class=model.tokenizer, 
+        model=model,
+        processing_class=model.tokenizer,
         configs=config,
-        datasets_dict=datasets_dict, 
+        datasets_dict=datasets_dict,
         env_and_gens_dict=env_and_gens_dict,
     )
-    
+
     # train or evaluate
     if config.run_cfg.mode == "train":
         runner.train()
     if config.run_cfg.mode == "evaluate":
         runner.evaluate()
+
 
 if __name__ == "__main__":
     main()
